35e345659477551a906acea050a1e6426c9e9fb6
MuneesDeepan28 <86399689+MuneesDeepan28@users.noreply.github.com>
2021-07-15 20:06:58 +0530

    adjust(js): variable name adjust and add reset concept

--- Form/assets/js/addemployeeDetails.js
+++ Form/assets/js/addemployeeDetails.js
@@ -7,11 +7,12 @@ function addEmployeeDetails() {
 	var empName = document.getElementById("employeeName").value;
 	var empGender = document.employeeDetails.gender.value;
 	var empOccupation = document.getElementById("employeeOccupation").value;
-	var Hobbies = document.querySelectorAll('input[type="checkbox"]:checked');
-	for (let employeeHobbies = 0; employeeHobbies < Hobbies.length; employeeHobbies++) {
-		empHobbies.push(Hobbies[employeeHobbies].value);
+	var gethobbies = document.querySelectorAll('input[type="checkbox"]:checked');
+	for (let employeeHobbies = 0; employeeHobbies < gethobbies.length; employeeHobbies++) {
+		empHobbies.push(gethobbies[employeeHobbies].value);
 	}
 	var empSalary = document.getElementById("employeeSalary").value;
+	var empEmailId = document.getElementById("emailId").value;
 	var getEmployeeDetails =
 	{
 		"EmployeeId": empid,
@@ -19,18 +20,18 @@ function addEmployeeDetails() {
 		"EmployeeGender": empGender,
 		"EmployeeOccupation": empOccupation,
 		"EmployeeHobbies": empHobbies,
-		"EmployeeSalary": empSalary
+		"EmployeeSalary": empSalary,
+		"EmployeeEmailId": empEmailId
 	}
 	if (localStorage.getItem('EmployeeDetail')) {
 		setlocalStoragevalue = JSON.parse(localStorage.getItem('EmployeeDetail'));
 		setlocalStoragevalue.push(getEmployeeDetails);
 		localStorage.setItem('EmployeeDetail', JSON.stringify(setlocalStoragevalue));
-		getlocalStoragevalue = JSON.parse(localStorage.getItem("EmployeeDetail"));
 	} else {
 		setlocalStoragevalue.push(getEmployeeDetails)
 		localStorage.setItem('EmployeeDetail', JSON.stringify([getEmployeeDetails]));
-		getlocalStoragevalue = JSON.parse(localStorage.getItem("EmployeeDetail"));
 	}
+	getlocalStoragevalue = JSON.parse(localStorage.getItem("EmployeeDetail"));
 	document.getElementById("displayEmployeeDetails").innerHTML = getlocalStoragevalue;
 	for (let displayDetails = 0; displayDetails < getlocalStoragevalue.length; displayDetails++) {
 		displayData += "<tr>";
@@ -40,7 +41,12 @@ function addEmployeeDetails() {
 		displayData += "<td>" + getlocalStoragevalue[displayDetails].EmployeeOccupation + "</td>";
 		displayData += "<td>" + getlocalStoragevalue[displayDetails].EmployeeHobbies + "</td>";
 		displayData += "<td>" + getlocalStoragevalue[displayDetails].EmployeeSalary + "</td>";
+		displayData += "<td>" + getlocalStoragevalue[displayDetails].EmployeeEmailId + "</td>";
 		displayData += "</tr>";
 	}
 	document.getElementById("displayEmployeeDetails").innerHTML = displayData;
+	resetForm();
+}
+function resetForm() {
+	document.getElementById('employeeDetail').reset();
 }
